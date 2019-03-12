a5baedda2a9ae9a28c084ad8b421e15a30138191
nugroholesmana <nugroholesmanap@gmail.com>
2019-03-12 14:36:47 +0700

    versi 1.0

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject ObjPanel;
+	public GameObject PanelKuisKita;
+
+	Animator AnimatorPanelKuisKita;
+
+	public static GameObject ClonePanelKuisKita;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ButtonClickPanelKuisKita(){
+		ClonePanelKuisKita = Instantiate (PanelKuisKita);
+		ClonePanelKuisKita.transform.SetParent (ObjPanel.transform,false);			
+	}
+
+	public void ButtonClickClosePanelKuisKita(){
+		Destroy (ClonePanelKuisKita);
+	}
+}
---
+++ Assets/Scripts/PrefabsPanel.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class PrefabsPanel : MonoBehaviour {
+	public void ButtonClickClosePanel(){
+		Destroy(MainMenu.ClonePanelKuisKita);
+	}
+}
